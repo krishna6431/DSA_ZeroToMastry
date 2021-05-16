2190bce49af420f42f116e4ff3faa933e6b6316d
Krishna Kant Verma <krishna6431@users.noreply.github.com>
2021-05-16 23:53:39 +0530

    add print all pos of element

---
+++ Section-10-Recursion-2/06_Print_all_Pos_of_element.cpp
@@ -0,0 +1,30 @@
+// All Rights Reserved &copy 2021
+// Code is Written by Krishna
+// Data Structure and Algorithms Series 2021
+
+#include <iostream>
+using namespace std;
+
+void printAllPos(int a[], int n, int x, int i)
+{
+    if (i == n)
+    {
+        return;
+    }
+    if (a[i] == x)
+    {
+        cout << i << " ";
+    }
+    printAllPos(a, n, x, i + 1);
+}
+
+int main()
+{
+    // #ifndef ONLINE_JUDGE
+    //     freopen("../input.txt", "r", stdin);
+    //     freopen("../output.txt", "w", stdout);
+    // #endif
+    int a[] = {5, 5, 6, 5, 6, 7};
+    printAllPos(a, 6, 10, 0);
+    return 0;
+}
